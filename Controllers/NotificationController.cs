10a3a3826c4270020de9481860d1ceb27bef4747
Alan Domingues <alanmdomingues@gmail.com>
2025-05-08 10:45:58 -0300

    fix: correção na prop NotificationService na NotificationController

--- Controllers/NotificationController.cs
+++ Controllers/NotificationController.cs
@@ -1,23 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SignalRServer.Services;
-using System.Threading.Tasks;
 
 namespace SignalRServer.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class NotificationController : Controller
+    public class NotificationController(NotificationService notificationService) : Controller
     {
-        public NotificationService _notificationService { get; set; }
-        public NotificationController(NotificationService notificationService)
-        {
-            _notificationService = notificationService;
-        }
-
         [HttpPost("{clientId}")]
         public async Task<IActionResult> SendNotification(string clientId)
         {
-            await _notificationService.SendNotification(clientId, "Hello World");
+            await notificationService.SendNotification(clientId, "Hello World");
             return Ok("Notification sent");
         }
     }
